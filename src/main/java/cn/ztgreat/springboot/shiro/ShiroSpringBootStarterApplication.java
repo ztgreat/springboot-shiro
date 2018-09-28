87d752c395c40496383f3ef0d9c093f4c6c51776
小张 <1170881778@qq.com>
2018-09-28 20:52:49 +0800

    init

---
+++ src/main/java/cn/ztgreat/springboot/shiro/ShiroSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package cn.ztgreat.springboot.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroSpringBootStarterApplication.class, args);
+	}
+}
---
+++ src/test/java/cn/ztgreat/springboot/shiro/ShiroSpringBootStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.ztgreat.springboot.shiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiroSpringBootStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
